d9a85c242e271fee1fd84ef61a365c32f3aafbcb
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-25 19:22:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,7 +26,11 @@ class Scraper
       #    course.description = post.css("p").text
       #     binding.pry
       #  end
-      self.get_page.css(".post")
+      
      end
+
+  def get_courses
+   self.get_page.css(".post")
+  end
 
 end
